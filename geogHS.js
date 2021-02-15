8c7aac067bdd9d0ae86ff4c67f393a1fc29caf13
s10205493 <s10205493@connect.np.edu.sg>
2021-02-15 11:32:32 +0800

    geog highscores (js)

---
+++ geogHS.js
@@ -0,0 +1,12 @@
+//reference to hs list
+const highScoresList = document.getElementById("highScoresList");
+
+//get the highscores from local storage
+const highScores = JSON.parse(localStorage.getItem("highScoresGeog")) || [];
+
+highScoresList.innerHTML = highScores
+//map => taking an array of items and converting them into something else
+.map(score => {
+    return `<li class = "high-score">${score.name} - ${score.score}</li>`;
+})
+.join("");
