321b4874805d8d43deecd59c008ac4bdbee73ed4
s10205493 <s10205493@connect.np.edu.sg>
2021-02-15 16:49:36 +0800

    Math Technology quiz -- completed

---
+++ techloadMath.js
@@ -0,0 +1,7 @@
+//after loading for less than a second, bring user to play game page
+
+setTimeout(myFunction, 3000);
+
+function myFunction(){
+  location.href = "techMathQuiz.html";
+}
---
+++ techloadMathHS.js
@@ -0,0 +1,6 @@
+
+setTimeout(myFunction, 2000);
+
+function myFunction(){
+  location.href = "techMathHS.html";
+}
